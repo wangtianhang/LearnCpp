25e41bad5b3f934bde348b90c893983da67b0412
wth <690879430@qq.com>
2019-07-14 23:18:34 +0800

    类模板

--- learnCppProject/LearnCppProject/LearnCppProject/src/TestTemplate.h
+++ learnCppProject/LearnCppProject/LearnCppProject/src/TestTemplate.h
@@ -9,6 +9,13 @@ void Swap(AnyType &a, AnyType &b)
 	b = temp;
 }
 
+template <class Type, class Type2>
+class TestTemplateClass
+{
+	Type m_t1;
+	Type2 m_t2;
+};
+
 void TestTemplate()
 {
 	std::cout << "TestTemplate ===============begin=================\n";
@@ -17,6 +24,8 @@ void TestTemplate()
 	int b = 6;
 	Swap(a, b);
 	Swap<int>(a, b);
+
+	TestTemplateClass<int, const char *> t = TestTemplateClass<int, const char *>();
 
 	std::cout << "TestTemplate ===============end=================\n";
 }

fbd904166456ac492fc06968ab7346125bec88bf
wth <690879430@qq.com>
2019-07-15 15:39:25 +0800

    补提交

--- learnCppProject/LearnCppProject/LearnCppProject/src/Learn3D/Mathf.h
+++ learnCppProject/LearnCppProject/LearnCppProject/src/Learn3D/Mathf.h
@@ -25,4 +25,39 @@ public:
 	{
 		return acos(f);
 	}
+
+	static bool Approximately(float a, float b)
+	{
+		return Mathf::Abs(b - a) < Mathf::Max(1E-06f * Mathf::Max(Mathf::Abs(a), Mathf::Abs(b)), 1.121039E-44f);
+	}
+
+	static float Asin(float f)
+	{
+		return asin(f);
+	}
+
+	static float Atan(float f)
+	{
+		return atan(f);
+	}
+
+	static float Atan2(float y, float x)
+	{
+		return atan2(y, x);
+	}
+
+
+
+
+
+
+
+
+
+
+
+	static float Max(float a, float b)
+	{
+		return (a <= b) ? b : a;
+	}
 };

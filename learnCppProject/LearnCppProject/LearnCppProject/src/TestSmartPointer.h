6db68a745a7580b8cae51d99cd53917dcfd2e38e
wth <690879430@qq.com>
2019-07-15 11:03:46 +0800

    补提交

--- learnCppProject/LearnCppProject/LearnCppProject/src/TestSmartPointer.h
+++ learnCppProject/LearnCppProject/LearnCppProject/src/TestSmartPointer.h
@@ -23,7 +23,70 @@ void TestAutoPtr()
 	std::cout << "The best is " << *pwin << std::endl;
 }
 
+// sharedptr最然不会导致崩溃 但是互相引用的话 可能导致无法释放 这又引出了weakptr的概念
+void TestSharedPtr()
+{
+	using namespace std;
+	shared_ptr<string> country[5] =
+	{
+		shared_ptr<string>(new string("USA")),
+		shared_ptr<string>(new string("CHN")),
+		shared_ptr<string>(new string("RUS")),
+		shared_ptr<string>(new string("FRA")),
+		shared_ptr<string>(new string("GRB"))
+	};
+
+	shared_ptr<string> pwin;
+	cout << pwin.use_count() << endl;//输出0
+	pwin = country[2];
+	/*使用shared_ptr时运行正常，因为shared_ptr采用引用计数，pwin和films[2]都指向同一块内存，
+	在释放空间时因为事先要判断引用计数值的大小因此不会出现多次删除一个对象的错误。
+
+	从名字share就可以看出了资源可以被多个指针共享，它使用计数机制来表明资源被几个指针共享。
+	可以通过成员函数use_count()来查看资源的所有者个数。
+	*/
+	cout << pwin.use_count() << endl;//输出2
+
+	for (int i = 0; i < 5; ++i)
+		cout << *country[i] << endl;
+	cout << "The best is " << *pwin << endl;
+}
+
+std::unique_ptr<std::string> TestReturnUniquePtr() {
+	return std::unique_ptr<std::string>(new std::string("RUS"));
+}
+
+void TestUniquePtr()
+{
+	using namespace std;
+	unique_ptr<string> pa(new string("CHN"));
+	//unique_ptr没有use_count()方法
+	unique_ptr<string> pb(new string("USA"));
+
+	pb = move(pa);
+	//p2=p1;//错误，不能直接用等于号
+	if (pa == nullptr)
+		cout << "pa现在为空" << endl;
+
+	cout << "*pb " << *pb << endl;//pb变成了“CHA”
+
+	string* ps = pb.release();//清空当前智能指针所指的资源对象,并返回指针
+	cout << "*ps " << *ps << endl;//ps变成了“CHA”
+
+	pa.reset(ps);//重置指向另一个对象
+	cout << "*pa " << *pa << endl;//pa变成了“CHA”
+
+	pb = TestReturnUniquePtr();//接收函数的返回值可以用等于号，因为使用了移动构造函数
+	cout << "*pb " << *pb << endl;//pb变成了“RUS”
+}
+
 void TestSmartPointer()
 {
+	std::cout << "TestSmartPointer ===============begin=================\n";
 	//TestAutoPtr();
+	TestSharedPtr();
+
+	TestUniquePtr();
+
+	std::cout << "TestSmartPointer ===============end=================\n";
 }

a7eb3357202282d5bfb37c604620b89dceefdc6a
wth <690879430@qq.com>
2019-07-15 15:53:18 +0800

    补math

--- learnCppProject/LearnCppProject/LearnCppProject/src/Learn3D/Mathf.h
+++ learnCppProject/LearnCppProject/LearnCppProject/src/Learn3D/Mathf.h
@@ -126,6 +126,111 @@ public:
 		return cos(f);
 	}
 
+	static float DeltaAngle(float current, float target)
+	{
+		float num = Mathf::Repeat(target - current, 360);
+		if (num > 180)
+		{
+			num -= 360;
+		}
+		return num;
+	}
+
+	static float Exp(float power)
+	{
+		return exp(power);
+	}
+
+	static float Floor(float f)
+	{
+		return floor(f);
+	}
+
+	static int FloorToInt(float f)
+	{
+		return (int)floor(f);
+	}
+
+	static float Gamma(float value, float absmax, float gamma)
+	{
+		bool flag = false;
+		if (value < 0)
+		{
+			flag = true;
+		}
+		float num = Mathf::Abs(value);
+		if (num > absmax)
+		{
+			return (!flag) ? num : (-num);
+		}
+		float num2 = Mathf::Pow(num / absmax, gamma) * absmax;
+		return (!flag) ? num2 : (-num2);
+	}
+
+	static float InverseLerp(float from, float to, float value)
+	{
+		if (from < to)
+		{
+			if (value < from)
+			{
+				return 0;
+			}
+			if (value > to)
+			{
+				return 1;
+			}
+			value -= from;
+			value /= to - from;
+			return value;
+		}
+		else
+		{
+			if (from <= to)
+			{
+				return 0;
+			}
+			if (value < to)
+			{
+				return 1;
+			}
+			if (value > from)
+			{
+				return 0;
+			}
+			return 1 - (value - to) / (from - to);
+		}
+	}
+
+	static float Lerp(float from, float to, float t)
+	{
+		return from + (to - from) * Mathf::Clamp01(t);
+	}
+
+	static float LerpAngle(float a, float b, float t)
+	{
+		float num = Mathf::Repeat(b - a, 360);
+		if (num > 180)
+		{
+			num -= 360;
+		}
+		return a + num * Mathf::Clamp01(t);
+	}
+
+	static float Log(float f)
+	{
+		return log(f);
+	}
+
+	static float Pow(float f, float p)
+	{
+		return pow(f, p);
+	}
+
+	static float Repeat(float t, float length)
+	{
+		return t - Mathf::Floor(t / length) * length;
+	}
+
 	static float Max(float a, float b)
 	{
 		return (a <= b) ? b : a;

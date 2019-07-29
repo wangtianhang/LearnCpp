c3a81eb75b18c54ecae69366fc08afda16ceceb5
wth <690879430@qq.com>
2019-07-29 15:21:03 +0800

    部分blinn-phong代码

--- learnCppProject/LearnCppProject/LearnOpenGL/src/Demo/Demo12.h
+++ learnCppProject/LearnCppProject/LearnOpenGL/src/Demo/Demo12.h
@@ -20,15 +20,16 @@ public:
 	GLuint m_rendering_program = 0;
 	//GLuint m_vertex_array_object = 0;
 
-	GLuint m_buffer;
+	//GLuint m_buffer;
 	GLuint m_vao;
+	int m_sphereDrawVertexCount = 0;
 
 	virtual void startup()
 	{
 		application::startup();
 
-		std::string vertex_shader_source = LoadTextFile("./Assets/shader/Demo6Vertex.txt");
-		std::string fragment_shader_source = LoadTextFile("./Assets/shader/Demo1Pixel.txt");
+		std::string vertex_shader_source = LoadTextFile("./Assets/shader/Demo12Vertex.txt");
+		std::string fragment_shader_source = LoadTextFile("./Assets/shader/Demo12Pixel.txt");
 
 		GLuint vertex = CreateShaderFromString(vertex_shader_source.c_str(), GL_VERTEX_SHADER, true);
 		GLuint pixel = CreateShaderFromString(fragment_shader_source.c_str(), GL_FRAGMENT_SHADER, true);
@@ -36,81 +37,83 @@ public:
 		//glCreateVertexArrays(1, &m_vertex_array_object);
 		//glBindVertexArray(m_vertex_array_object);
 
-		TestBufferWithVAO();
+		//TestBufferWithVAO();
+		
+		m_vao = GLUtil::CreateSphereVAO(m_sphereDrawVertexCount);
 	}
 
-	void TestBufferWithVAO()
-	{
-		static const GLfloat vertex_positions[] =
-		{
-			-0.25f,  0.25f, -0.25f,
-			-0.25f, -0.25f, -0.25f,
-			 0.25f, -0.25f, -0.25f,
-
-			 0.25f, -0.25f, -0.25f,
-			 0.25f,  0.25f, -0.25f,
-			-0.25f,  0.25f, -0.25f,
-
-			 0.25f, -0.25f, -0.25f,
-			 0.25f, -0.25f,  0.25f,
-			 0.25f,  0.25f, -0.25f,
-
-			 0.25f, -0.25f,  0.25f,
-			 0.25f,  0.25f,  0.25f,
-			 0.25f,  0.25f, -0.25f,
-
-			 0.25f, -0.25f,  0.25f,
-			-0.25f, -0.25f,  0.25f,
-			 0.25f,  0.25f,  0.25f,
-
-			-0.25f, -0.25f,  0.25f,
-			-0.25f,  0.25f,  0.25f,
-			 0.25f,  0.25f,  0.25f,
-
-			-0.25f, -0.25f,  0.25f,
-			-0.25f, -0.25f, -0.25f,
-			-0.25f,  0.25f,  0.25f,
-
-			-0.25f, -0.25f, -0.25f,
-			-0.25f,  0.25f, -0.25f,
-			-0.25f,  0.25f,  0.25f,
-
-			-0.25f, -0.25f,  0.25f,
-			 0.25f, -0.25f,  0.25f,
-			 0.25f, -0.25f, -0.25f,
-
-			 0.25f, -0.25f, -0.25f,
-			-0.25f, -0.25f, -0.25f,
-			-0.25f, -0.25f,  0.25f,
-
-			-0.25f,  0.25f, -0.25f,
-			 0.25f,  0.25f, -0.25f,
-			 0.25f,  0.25f,  0.25f,
-
-			 0.25f,  0.25f,  0.25f,
-			-0.25f,  0.25f,  0.25f,
-			-0.25f,  0.25f, -0.25f
-		};
-
-		// Create the vertex array object
-		glCreateVertexArrays(1, &m_vao);
-		// Get create two buffers
-		glCreateBuffers(1, &m_buffer);
-		// Initialize the first buffer
-		glNamedBufferStorage(m_buffer, sizeof(vertex_positions), vertex_positions, 0);
-		// Bind it to the vertex array - offset zero, stride = sizeof(vec3)
-		int tmp = sizeof(Vector3);
-		// tmp == 12 ....
-		glVertexArrayVertexBuffer(m_vao, 0, m_buffer, 0, sizeof(Vector3));
-		// Tell OpenGL what the format of the attribute is
-		glVertexArrayAttribFormat(m_vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
-		// Tell OpenGL which vertex buffer binding to use for this attribute
-		glVertexArrayAttribBinding(m_vao, 0, 0);
-		// Enable the attribute
-		glEnableVertexArrayAttrib(m_vao, 0);
-
-
-	}
+// 	void TestBufferWithVAO()
+// 	{
+// 		static const GLfloat vertex_positions[] =
+// 		{
+// 			-0.25f,  0.25f, -0.25f,
+// 			-0.25f, -0.25f, -0.25f,
+// 			 0.25f, -0.25f, -0.25f,
+// 
+// 			 0.25f, -0.25f, -0.25f,
+// 			 0.25f,  0.25f, -0.25f,
+// 			-0.25f,  0.25f, -0.25f,
+// 
+// 			 0.25f, -0.25f, -0.25f,
+// 			 0.25f, -0.25f,  0.25f,
+// 			 0.25f,  0.25f, -0.25f,
+// 
+// 			 0.25f, -0.25f,  0.25f,
+// 			 0.25f,  0.25f,  0.25f,
+// 			 0.25f,  0.25f, -0.25f,
+// 
+// 			 0.25f, -0.25f,  0.25f,
+// 			-0.25f, -0.25f,  0.25f,
+// 			 0.25f,  0.25f,  0.25f,
+// 
+// 			-0.25f, -0.25f,  0.25f,
+// 			-0.25f,  0.25f,  0.25f,
+// 			 0.25f,  0.25f,  0.25f,
+// 
+// 			-0.25f, -0.25f,  0.25f,
+// 			-0.25f, -0.25f, -0.25f,
+// 			-0.25f,  0.25f,  0.25f,
+// 
+// 			-0.25f, -0.25f, -0.25f,
+// 			-0.25f,  0.25f, -0.25f,
+// 			-0.25f,  0.25f,  0.25f,
+// 
+// 			-0.25f, -0.25f,  0.25f,
+// 			 0.25f, -0.25f,  0.25f,
+// 			 0.25f, -0.25f, -0.25f,
+// 
+// 			 0.25f, -0.25f, -0.25f,
+// 			-0.25f, -0.25f, -0.25f,
+// 			-0.25f, -0.25f,  0.25f,
+// 
+// 			-0.25f,  0.25f, -0.25f,
+// 			 0.25f,  0.25f, -0.25f,
+// 			 0.25f,  0.25f,  0.25f,
+// 
+// 			 0.25f,  0.25f,  0.25f,
+// 			-0.25f,  0.25f,  0.25f,
+// 			-0.25f,  0.25f, -0.25f
+// 		};
+// 
+// 		// Create the vertex array object
+// 		glCreateVertexArrays(1, &m_vao);
+// 		// Get create two buffers
+// 		glCreateBuffers(1, &m_buffer);
+// 		// Initialize the first buffer
+// 		glNamedBufferStorage(m_buffer, sizeof(vertex_positions), vertex_positions, 0);
+// 		// Bind it to the vertex array - offset zero, stride = sizeof(vec3)
+// 		int tmp = sizeof(Vector3);
+// 		// tmp == 12 ....
+// 		glVertexArrayVertexBuffer(m_vao, 0, m_buffer, 0, sizeof(Vector3));
+// 		// Tell OpenGL what the format of the attribute is
+// 		glVertexArrayAttribFormat(m_vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+// 		// Tell OpenGL which vertex buffer binding to use for this attribute
+// 		glVertexArrayAttribBinding(m_vao, 0, 0);
+// 		// Enable the attribute
+// 		glEnableVertexArrayAttrib(m_vao, 0);
+// 
+// 
+// 	}
 
 	virtual void render(double currentTime)
 	{
@@ -143,12 +146,25 @@ public:
 		float farPlane = 1000;
 		Matrix4x4 project = Matrix4x4::Perspective(fov, aspect, nearPlane, farPlane);
 		Matrix4x4 mvp = project * view * model_localToWorld;
+		Matrix4x4 mv = view * model_localToWorld;
 		//Matrix4x4 mvp = model_localToWorld * view * project;
 		glUseProgram(m_rendering_program);
-		GLuint mvpLocation = glGetUniformLocation(m_rendering_program, "mvp_matrix");
-		float mvpMatrixArray[16];
-		mvp.GetMatrixArray(mvpMatrixArray);
-		glUniformMatrix4fv(mvpLocation, 1, true, mvpMatrixArray);
+
+		
+		GLuint mvLocation = glGetUniformLocation(m_rendering_program, "mv_matrix");
+		float mvMatrixArray[16];
+		mv.GetMatrixArray(mvMatrixArray);
+		glUniformMatrix4fv(mvLocation, 1, true, mvMatrixArray);
+
+		GLuint viewLocation = glGetUniformLocation(m_rendering_program, "view_matrix");
+		float viewMatrixArray[16];
+		view.GetMatrixArray(viewMatrixArray);
+		glUniformMatrix4fv(viewLocation, 1, true, viewMatrixArray);
+
+		GLuint projLocation = glGetUniformLocation(m_rendering_program, "proj_matrix");
+		float projMatrixArray[16];
+		project.GetMatrixArray(projMatrixArray);
+		glUniformMatrix4fv(projLocation, 1, true, projMatrixArray);
 
 		glEnable(GL_CULL_FACE);
 		glCullFace(GL_BACK);
@@ -157,7 +173,7 @@ public:
 
 		// 这种先bind再draw很蛋疼。。
 		glBindVertexArray(m_vao);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glDrawElements(GL_TRIANGLES, m_sphereDrawVertexCount, GL_UNSIGNED_SHORT, 0);
 		//glDrawArrays(m_vertex_array_object, )
 
 
@@ -169,6 +185,6 @@ public:
 
 		glDeleteProgram(m_rendering_program);
 		glDeleteVertexArrays(1, &m_vao);
-		glDeleteBuffers(1, &m_buffer);
+		//glDeleteBuffers(1, &m_buffer);
 	}
 };

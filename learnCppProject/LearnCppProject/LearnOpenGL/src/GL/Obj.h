1d79c2aff3a40a6f075dde286932b743a19e7fe9
wth <690879430@qq.com>
2019-07-31 11:58:58 +0800

    添加obj类

--- learnCppProject/LearnCppProject/LearnOpenGL/src/GL/Obj.h
+++ learnCppProject/LearnCppProject/LearnOpenGL/src/GL/Obj.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "gl3w.h"
+#include "../Graphic/MeshData.h"
 
 struct Obj
 {
 public:
-	bool m_isIndex = true;
-	GLuint m_vao = 0;
-	int m_vertexCount = 0; // index数量
+	//bool m_isIndex = true;
+	//GLuint m_vao = 0;
+	//int m_vertexCount = 0; // index数量
+	MeshData m_meshData;
 	
 	bool m_isOpaque = true;
 	GLenum m_blendFactor1 = GL_SRC_ALPHA;
@@ -19,24 +21,82 @@ public:
 
 	bool m_cullFace = true;
 	GLenum m_cullFaceType = GL_BACK;
-	GLenum m_frontFace = GL_CW;
+	//GLenum m_frontFace = GL_CW;
 
 	bool m_useOutProgram = false;
 	GLuint m_renderProgram = 0;
 	GLuint m_outProgram = 0;
 
-	void InitShader(std::string vertexShader, std::string pixelShader)
-	{
+// 	void InitShader(std::string vertexShader, std::string pixelShader)
+// 	{
+// 
+// 	}
 
-	}
-
-	void InitVAO(GLuint vao, int vertexCount, bool isIndex)
-	{
-
-	}
+// 	void InitVAO(GLuint vao, int vertexCount, bool isIndex)
+// 	{
+// 
+// 	}
 
 	void RenderObj()
 	{
+		if (m_isOpaque)
+		{
+			glDisable(GL_BLEND);
+		}
+		else
+		{
+			glEnable(GL_BLEND);
+			glBlendFunc(m_blendFactor1, m_blendFactor2);
+		}
 
+		if (m_zwrite)
+		{
+			glDepthMask(true);
+		}
+		else
+		{
+			glDepthMask(false);
+		}
+
+		if (m_ztest)
+		{
+			glEnable(GL_DEPTH_TEST);
+			glDepthFunc(m_zTestOp);
+		}
+		else
+		{
+			glDisable(GL_DEPTH_TEST);
+		}
+
+		if (m_cullFace)
+		{
+			glEnable(GL_CULL_FACE);
+			glCullFace(m_cullFaceType);
+		}
+		else
+		{
+			glDisable(GL_CULL_FACE);
+		}
+
+		
+		if (m_useOutProgram)
+		{
+			glUseProgram(m_outProgram);
+		}
+		else
+		{
+			glUseProgram(m_renderProgram);
+		}
+		
+		glFrontFace(m_meshData.m_frontFace);
+		glBindVertexArray(m_meshData.m_vao);
+		if (m_meshData.m_isIndex)
+		{
+			glDrawElements(GL_TRIANGLES, m_meshData.drawVerticesCount, GL_UNSIGNED_SHORT, 0);
+		}
+		else
+		{
+			glDrawArrays(GL_TRIANGLES, 0, m_meshData.drawVerticesCount);
+		}
 	}
 };

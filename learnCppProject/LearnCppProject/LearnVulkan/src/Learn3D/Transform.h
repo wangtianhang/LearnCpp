eb7934a7d5e203024364c3a7d9f63c50ef505401
wth <690879430@qq.com>
2019-11-22 14:14:30 +0800

    挪实现到cpp完成

--- learnCppProject/LearnCppProject/LearnVulkan/src/Learn3D/Transform.h
+++ learnCppProject/LearnCppProject/LearnVulkan/src/Learn3D/Transform.h
@@ -1,12 +1,10 @@
 #pragma once
 
 #include <vector>
-
-#include "./Vector3.h"
 #include "./Quaternion.h"
-#include "./Matrix4x4.h"
-#include "../GUtil.h"
+#include "./Vector3.h"
 
+struct Matrix4x4;
 class Transform
 {
 private:
@@ -29,409 +27,92 @@ private:
 public:
 	std::string m_name;
 
+	static void Test();
 
-	static void Test()
-	{
-		Transform test;
-		//Vector3 euler = Vector3(-87.729256, 0.000000, 0.000000);
-		Vector3 euler = Vector3(-88, 0.000000, 0.000000);
-		GUtil::Log(euler.toString());
-		test.SetEulerAngles(euler);
-		Vector3 euler2 = NormalizeAngles(test.GetEulerAngles());
-		GUtil::Log(euler2.toString());
+	static Vector3 NormalizeAngles(Vector3 angles);
 
-		Quaternion qua = Quaternion::Euler(euler);
-		Vector3 euler3 = qua.eulerAngles();
-		GUtil::Log(NormalizeAngles(euler3).toString());
-	}
+	static float NormalizeAngle(float angle);
 
-	static Vector3 NormalizeAngles(Vector3 angles)
-	{
-		angles.x = NormalizeAngle(angles.x);
-		angles.y = NormalizeAngle(angles.y);
-		angles.z = NormalizeAngle(angles.z);
-		return angles;
-	}
+	Vector3 GetPosition();
 
-	static float NormalizeAngle(float angle)
-	{
-		while (angle > 180)
-			angle -= 360;
-		while (angle < -180)
-			angle += 360;
-		return angle;
-	}
+	void SetPosition(Vector3 value);
 
-	Vector3 GetPosition()
-	{
-		if (m_isWorldPositionNeedUpdate)
-		{
-			m_worldPosition = convertLocalToWorldPosition(Vector3::zero());
-			m_isWorldPositionNeedUpdate = false;
-		}
-		return m_worldPosition;
-	}
+	Vector3 GetLocalPosition();
 
-	void SetPosition(Vector3 value)
-	{
-		m_isWorldPositionNeedUpdate = true;
-		m_localPostion = convertWorldToLocalPosition(value);
-		if (m_name == "Bip001 Pelvis")
-		{
-			int test = 0;
-		}
-		SetChildNeedUpdate();
-	}
+	void SetLocalPosition(Vector3 value);
 
-	Vector3 GetLocalPosition()
-	{
-		return m_localPostion;
-	}
+	static Vector3 Divide(Vector3 a, Vector3 b);
 
-	void SetLocalPosition(Vector3 value)
-	{
-		m_isWorldPositionNeedUpdate = true;
-		if (m_name == "Bip001 Pelvis")
-		{
-			int test = 0;
-		}
-		m_localPostion = value;
-		SetChildNeedUpdate();
-	}
+	Vector3 convertWorldToLocalPosition(Vector3 worldPos);
 
-	static Vector3 Divide(Vector3 a, Vector3 b)
-	{
-		return Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
-	}
+	Vector3 convertLocalToWorldPosition(Vector3 localPos);
 
-	Vector3 convertWorldToLocalPosition(Vector3 worldPos)
-	{
-		if (m_needParentUpdate)
-		{
-			_updateFromParent();
-		}
+	void SetChildNeedUpdate();
 
-		Quaternion inverse = Quaternion::Inverse(GetParentOrientation());
-		Vector3 divide = Divide(worldPos - GetParentPosition(), GetParentScale());
-		return inverse * divide;
-	}
+	void SetNeedUpdateParent();
 
-	Vector3 convertLocalToWorldPosition(Vector3 localPos)
-	{
-		if (m_needParentUpdate)
-		{
-			_updateFromParent();
-		}
-		//Matrix4x4 matrix = Matrix4x4.TRS(_getDerivedPosition(), _getDerivedOrientation(), _getDerivedScale());
-		Matrix4x4 matrix = GetSelfTransform();
-		return matrix.MultiplyPoint(localPos);
-	}
+	void _updateFromParent();
 
-	void SetChildNeedUpdate()
-	{
-		//if(m_parent == null)
-		{
-			m_needParentUpdate = true;
-			//m_isWorldPositionNeedUpdate = true;
-		}
+	Quaternion GetParentOrientation();
 
-		for(int i = 0; i < m_childList.size(); ++i)
-		{
-			Transform * child = m_childList[i];
-			child->SetNeedUpdateParent();
-		}
-	}
+	Vector3 GetParentScale();
 
-	void SetNeedUpdateParent()
-	{
-		m_needParentUpdate = true;
-		//m_isWorldPositionNeedUpdate = true;
+	Vector3 GetParentPosition();
 
-		SetChildNeedUpdate();
-	}
+	Quaternion _getDerivedOrientation();
 
-	void _updateFromParent()
-	{
-		if (m_parent != NULL)
-		{
-			Quaternion parentOrientation = m_parent->_getDerivedOrientation();
-			m_DerivedOrientation = parentOrientation * m_localRotation;
-			//Vector3 parentScale = m_parent->GetLossyScale();
-			m_DerivedScale = m_parent->GetLossyScale();
-			// Change position vector based on parent's orientation & scale
-			m_DerivedPosition = parentOrientation * Vector3::Scale(m_DerivedScale, m_localPostion);
-			// Add altered position vector to parents
-			m_DerivedPosition = m_DerivedPosition + m_parent->_getDerivedPosition();
-		}
-		else
-		{
-			m_DerivedOrientation = m_localRotation;
-			m_DerivedPosition = m_localPostion;
-			m_DerivedScale = Vector3::one();
-		}
+	Vector3 _getDerivedScale();
 
-		m_needParentUpdate = false;
-		m_isWorldPositionNeedUpdate = true;
-	}
+	Vector3 _getDerivedPosition();
 
-	Quaternion GetParentOrientation()
-	{
-		if (m_parent != NULL)
-		{
-			return m_parent->_getDerivedOrientation();
-		}
-		else
-		{
-			return Quaternion::identity();
-		}
-	}
+	Quaternion GetRotation();
 
-	Vector3 GetParentScale()
-	{
-		if (m_parent != NULL)
-		{
-			return m_parent->_getDerivedScale();
-		}
-		else
-		{
-			return Vector3::one();
-		}
-	}
+	void SetRotation(Quaternion value);
 
-	Vector3 GetParentPosition()
-	{
-		if (m_parent != NULL)
-		{
-			return m_parent->_getDerivedPosition();
-		}
-		else
-		{
-			return Vector3::zero();
-		}
-	}
+	Vector3 GetLocalEulerAngles();
 
-	Quaternion _getDerivedOrientation()
-	{
-		//         if(m_parent == null)
-		//         {
-		//             return m_localRotation;
-		//         }
-		//         else
-		//         {
-		if (m_needParentUpdate)
-		{
-			_updateFromParent();
-		}
-		return m_DerivedOrientation;
-		//        }
-	}
+	void SetLocalEulerAngles(Vector3 value);
 
-	Vector3 _getDerivedScale()
-	{
-		//         if(m_parent == null)
-		//         {
-		//             return m_localScale;
-		//         }
-		//         else
-		//         {
-		if (m_needParentUpdate)
-		{
-			_updateFromParent();
-		}
-		return m_DerivedScale;
-		//        }
-	}
+	Vector3 GetEulerAngles();
 
-	Vector3 _getDerivedPosition()
-	{
-		//         if(m_parent == null)
-		//         {
-		//             return m_localPostion;
-		//         }
-		//         else
-		//         {
-		if (m_needParentUpdate)
-		{
-			_updateFromParent();
-		}
-		return m_DerivedPosition;
-		//        }
-	}
+	void SetEulerAngles(Vector3 value);
 
-	Quaternion GetRotation()
-	{
-		return convertLocalToWorldOrientation(m_localRotation);
-	}
+	Quaternion convertLocalToWorldOrientation(Quaternion localOrientation);
 
-	void SetRotation(Quaternion value)
-	{
-		m_localRotation = convertWorldToLocalOrientation(value);
-		SetChildNeedUpdate();
-	}
+	Quaternion convertWorldToLocalOrientation(Quaternion worldOrientation);
 
-	Vector3 GetLocalEulerAngles()
-	{
-		return m_localRotation.eulerAngles();
-	}
+	Transform * GetParent();
 
-	void SetLocalEulerAngles(Vector3 value)
-	{
-		m_localRotation = Quaternion::Euler(value);
-		SetChildNeedUpdate();
-	}
+	void SetParent(Transform * value);
 
-	Vector3 GetEulerAngles()
-	{
-		return GetRotation().eulerAngles();
-	}
+	Quaternion GetLocalRotation();
 
-	void SetEulerAngles(Vector3 value)
-	{
-		SetRotation(Quaternion::Euler(value));
-	}
+	void SetLocalRotation(Quaternion value);
 
-	Quaternion convertLocalToWorldOrientation(Quaternion localOrientation)
-	{
-		if (m_needParentUpdate)
-		{
-			_updateFromParent();
-		}
-		return GetParentOrientation() * localOrientation;
-	}
+	Vector3 GetLocalScale();
 
-	Quaternion convertWorldToLocalOrientation(Quaternion worldOrientation)
-	{
-		if (m_needParentUpdate)
-		{
-			_updateFromParent();
-		}
-		return Quaternion::Inverse(GetParentOrientation()) * worldOrientation;
-	}
+	void SetLocalScale(Vector3 value);
 
-	Transform * GetParent()
-	{
-		return m_parent;
-	}
+	Vector3 GetLossyScale();
 
-	void SetParent(Transform * value)
-	{
-		Vector3 cacheWorldPos = GetPosition();
-		Quaternion cacheWorldQua = GetRotation();
-		Vector3 cacheWorldScale = GetLossyScale();
+	void SetLossyScale(Vector3 value);
 
-		if (m_parent != NULL)
-		{
-			//m_parent->m_childList.erase(this);
-			for (int i = 0; i < m_parent->m_childList.size(); ++i)
-			{
-				Transform * iter = m_parent->m_childList[i];
-				if (iter == this)
-				{
-					m_parent->m_childList.erase(m_parent->m_childList.begin() + i);
-					break;
-				}
-			}
-		}
+	Matrix4x4 GetSelfTransform();
 
-		m_parent = value;
+	Matrix4x4 GetLocalToWorldMatrix();
 
-		if (m_parent != NULL)
-		{
-			m_parent->m_childList.push_back(this);
-		}
+	Matrix4x4 GetWorldToLocalMatrix();
 
-		SetChildNeedUpdate();
+	Vector3 GetForward();
 
-		SetPosition(cacheWorldPos);
-		SetRotation(cacheWorldQua);
-		SetLossyScale(cacheWorldScale);
-	}
+	void SetForward(Vector3 value);
 
-	Quaternion GetLocalRotation()
-	{
-		return m_localRotation;
-	}
+	Vector3 GetRight();
 
-	void SetLocalRotation(Quaternion value)
-	{
-		m_localRotation = value;
-		SetChildNeedUpdate();
-	}
+	void SetRight(Vector3 value);
 
-	Vector3 GetLocalScale()
-	{
-		return m_localScale;
-	}
+	Vector3 GetUp();
 
-	void SetLocalScale(Vector3 value)
-	{
-		m_localScale = value;
-		if (m_localScale.x > 360 && m_localScale.x < 362)
-		{
-			int test = 0;
-		}
-		SetChildNeedUpdate();
-	}
-
-	Vector3 GetLossyScale()
-	{
-		return Vector3::Scale(m_DerivedScale, m_localScale);
-	}
-
-	void SetLossyScale(Vector3 value)
-	{
-		m_localScale = Divide(value, m_DerivedScale);
-		if (m_localScale.x > 360 && m_localScale.x < 362)
-		{
-			int test = 0;
-		}
-		SetChildNeedUpdate();
-	}
-
-	Matrix4x4 GetSelfTransform()
-	{
-		Matrix4x4 matrix = Matrix4x4::TRS(_getDerivedPosition(), _getDerivedOrientation(), GetLossyScale());
-		return matrix;
-	}
-
-	Matrix4x4 GetLocalToWorldMatrix()
-	{
-		return GetSelfTransform();
-	}
-
-	Matrix4x4 GetWorldToLocalMatrix()
-	{
-		return GetSelfTransform().inverse();
-	}
-
-	Vector3 GetForward()
-	{
-		return GetRotation() * Vector3::forward();
-	}
-
-	void SetForward(Vector3 value)
-	{
-		SetRotation(Quaternion::LookRotation(value));
-	}
-
-	Vector3 GetRight()
-	{
-		return GetRotation() * Vector3::right();
-	}
-
-	void SetRight(Vector3 value)
-	{
-		SetRotation(Quaternion::FromToRotation(Vector3::right(), value));
-	}
-
-	Vector3 GetUp()
-	{
-		return GetRotation() * Vector3::up();
-	}
-
-	void SetUp(Vector3 value)
-	{
-		SetRotation(Quaternion::FromToRotation(Vector3::up(), value));
-	}
+	void SetUp(Vector3 value);
 
 };
